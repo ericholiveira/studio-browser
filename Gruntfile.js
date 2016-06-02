63df1478da93ac3fde97ea21bab2d57b5a13e30d
Erich Oliveira <ericholiveira10@gmail.com>
2016-06-01 23:25:22 -0300

    adding basic documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ var grunt = require("grunt");
 grunt.loadNpmTasks('grunt-contrib-jshint');
 grunt.loadNpmTasks('grunt-release');
 grunt.loadNpmTasks('grunt-mocha-test');
+grunt.loadNpmTasks('grunt-jsdoc');
 grunt.initConfig({
 	jshint: {
 		all: ['src/**/*.js', '*.js', 'tests/**/*.js'],
@@ -24,9 +25,19 @@ grunt.initConfig({
 			npm: true,
 			npmTag: "<%= version %>"
 		}
-	}
+	},
+	jsdoc : {
+        dist : {
+            src: ['src/*.js','README.md'],
+            options: {
+                destination: '.documentation',
+                template : "node_modules/ink-docstrap/template",
+              	configure : "node_modules/ink-docstrap/template/jsdoc.conf.json"
+            }
+        }
+    }
 });
 grunt.registerTask("test", ["mochaTest:test"]);
 grunt.registerTask("all", ["jshint", "test"]);
-grunt.registerTask("default", ["all", "watch"]);
+grunt.registerTask("default", ["all"]);
 grunt.registerTask("prod", ["all", "release"]);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,26 @@ var DEFAULT_NAMESPACE = '__browser';
 
 var browserServiceFactory = require('./browser_service_factory');
 var serverFactory = require('./socket_io_server');
-
-module.exports = function(options){
+/**
+ * Plugin for socketio browser access
+ * @constructor
+ * @author Erich Oliveira
+ * @public
+ * @param {Object} options
+ * @param {String} options.defaultNamespace the module name which will prepend the service automatically instiated for browser access. Defaults to __browser
+ * @param {String} options.serviceCallEventName The name of the channel used to communicate the services. Defaults to __studio_service_call
+ * @param {String} options.socketIO The socketIO object used as the server, if none is provided it creates a new server binding to options.port
+ * @param {String} options.port If no options.socketIO is provided, the new server will bind to this port. Defaults to 3000
+ * @default {
+		defaultNamespace:'__browser',
+		serviceCallEventName:'__studio_service_call',
+		port:3000
+ 	}
+ * @example 
+   var browserPlugin = require('studio-browser');
+   Studio.use(browserPlugin());
+ */
+function Plugin(options){
 	"use strict";
 	options = options || {};
 	options.defaultNamespace = options.defaultNamespace || DEFAULT_NAMESPACE;
@@ -19,4 +37,5 @@ module.exports = function(options){
 		});
 	};
 }
+module.exports = Plugin;
 	
